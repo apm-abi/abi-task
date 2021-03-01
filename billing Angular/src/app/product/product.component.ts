9d9b8a8ec3c1db894eb6bff830686a73ad33b6d8
apm-abi <abilash@isquarebs.com>
2021-03-01 18:59:09 +0530

    update

--- billing Angular/src/app/product/product.component.ts
+++ billing Angular/src/app/product/product.component.ts
@@ -3,9 +3,53 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent  {
+export class ProductComponent {
+  fname = '';
+  fprice = '';
+  row = 0;
+  col = 0;
 
+  rows = [];
+  cols = [];
 
+  list = [];
+  constructor() {
+    const li = localStorage.getItem('lis');
+    if (li) {
+      this.list = JSON.parse(li);
+    }
+  }
+  // tslint:disable-next-line:typedef
+  add() {
+    const h = {
+      n: this.fname,
+      p: this.fprice
+    };
+    this.list.push(h);
+    localStorage.setItem('lis', JSON.stringify(this.list));
+  }
+  // tslint:disable-next-line:typedef
+  remove(list) {
+    const index = this.list.indexOf(list);
+    // tslint:disable-next-line:align
+    this.list.splice(index, 1);
+  }
+  // tslint:disable-next-line:align
+  tab() {
+    this.rows = [];
+    for (let i = 0; i < this.row; i++) {
+      this.rows.push('');
+    }
+    this.cols = [];
+    for (let k = 0; k < this.col; k++) {
+      this.rows.push('');
+    }    
+  }
 }
+
+
+
+
+
--- billing Angular/src/app/productmodule/productmodule.module.ts
+++ billing Angular/src/app/productmodule/productmodule.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import { ProductComponent } from '../product/product.component';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [ ProductComponent  ],
-  imports: [ CommonModule, MatInputModule, MatButtonModule],
+  imports: [ CommonModule, MatInputModule, MatButtonModule, FormsModule, MatTableModule],
   exports: [ ProductComponent ]
 })
 export class ProductModule { }
