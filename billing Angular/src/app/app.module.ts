59aa76a3c497b971529d23ee909bd65cfa172bb9
apm-abi <abilash@isquarebs.com>
2021-03-01 13:03:31 +0530

    update

--- billing Angular/src/app/app.module.ts
+++ billing Angular/src/app/app.module.ts
@@ -6,20 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
 import { FormsModule} from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {MatMenuModule} from '@angular/material/menu';
 import { ProductModule } from './productmodule/productmodule.module';
-
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
   ],
   imports: [
-    BrowserModule, FormsModule, CommonModule, MatMenuModule, ProductModule,
-    AppRoutingModule, MatIconModule, MatTableModule,
+    BrowserModule, FormsModule, CommonModule, ProductModule,
+    AppRoutingModule, MatIconModule,
     BrowserAnimationsModule
   ],
   providers: [],
--- billing Angular/src/app/productmodule/productmodule.module.ts
+++ billing Angular/src/app/productmodule/productmodule.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatInputModule} from '@angular/material/input';
 import { ProductComponent } from '../product/product.component';
-
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [ ProductComponent  ],
-  imports: [ CommonModule, MatInputModule ],
+  imports: [ CommonModule, MatInputModule, MatButtonModule],
   exports: [ ProductComponent ]
 })
 export class ProductModule { }
